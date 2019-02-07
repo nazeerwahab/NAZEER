4e5fddc07bad77a0f3f2804f9b0d474cdc8e93e4
nazeerwahab <42862748+nazeerwahab@users.noreply.github.com>
2019-02-07 14:19:46 +0530

    Update greaterthreeno.py

--- greaterthreeno.py
+++ greaterthreeno.py
@@ -1,12 +1,11 @@
 a = int(input(""))
 b = int(input(""))
 c = int(input(""))
-if(a>b) and (a>c):
-  largest=a
-elif(b>a) and (b>c):
-    largest=b
-elif(c>a) and (c>b):
-  largest=c
-print(largest)
+if(a>b and a>c):
+  print(a)
+elif(b>a and b>c):
+    print(b)
+elif(c>a and c>b):
+  print(c)
 else:
   print("Invalid")
