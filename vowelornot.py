87f1f3f144008f61b156d2916c57ebef12498c58
nazeerwahab <42862748+nazeerwahab@users.noreply.github.com>
2019-02-07 13:57:12 +0530

    Update vowelornot.py

--- vowelornot.py
+++ vowelornot.py
@@ -2,7 +2,7 @@ ch = input("")
 
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print("vowel")
+    print("Vowel")
 else:
     print("Consonant")
 
