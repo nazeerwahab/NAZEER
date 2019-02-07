da11d11a1f0d2c8033d6d60a7813a4883f0dffb7
nazeerwahab <42862748+nazeerwahab@users.noreply.github.com>
2019-02-07 14:50:58 +0530

    Update sumofnaturalno.py

--- sumofnaturalno.py
+++ sumofnaturalno.py
@@ -1,9 +1,5 @@
-num = int(input("Enter a NUMBER: "))
-if num < 0:
-  print("Enter a POSITIVE NUMBER")
-else:
-  sum = 0
-  while(num > 0):
-    sum += num
-    num -= 1
-    print("The SUM is",sum)
+n=int(input())
+a=0
+for i in range(1,n+1):
+  a=a+i
+print(a)
