78a20bbe7b80df8b28a3a7d00ab5215e0880c154
nazeerwahab <42862748+nazeerwahab@users.noreply.github.com>
2019-02-07 11:58:52 +0530

    Update greaterthreeno.py

--- greaterthreeno.py
+++ greaterthreeno.py
@@ -1,10 +1,10 @@
-a = (int(input("Enter 1 NUMBER:"))
-b = (int(input("Enter 2 NUMBER:"))
-c =(int(input("Enter 3 NUMBER:"))
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
 if(a>b) and (a>c):
   largest=a
 elif(b>a) and (b>c):
     largest=b
 else:
       largest=c
-print("The LARGEST NUMBER :",largest)
+print(largest)
